484ebc7affb644898ce41da3751558a6738cf165
Diego Sánchez <diego.vcnte@gmail.com>
2023-11-24 23:21:10 -0300

    final

--- src/app/generar-qr/generar-qr.page.ts
+++ src/app/generar-qr/generar-qr.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../servicios/autentication.service';
 import * as QRCode from 'qrcode'; // Importa la librería QRCode
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-generar-qr',
@@ -24,5 +25,20 @@ export class GenerarQRPage implements OnInit {
         this.qrCodeData = url; // Almacena la URL del código QR generada
       }
     });
+  } // Método para iniciar el escaneo al presionar el botón
+  async iniciarEscaneo() {
+    try {
+      const result = await BarcodeScanner.startScan();
+      if (result.hasContent) {
+        // Maneja el resultado del escaneo como desees
+        console.log('Contenido del QR escaneado:', result.content);
+
+        // Actualiza la propiedad qrEscaneado del usuario correspondiente
+        this.authService.actualizarQREscaneado(result.content);
+      }
+    } catch (error) {
+      console.error('Error al escanear:', error);
+    }
   }
 }
+
